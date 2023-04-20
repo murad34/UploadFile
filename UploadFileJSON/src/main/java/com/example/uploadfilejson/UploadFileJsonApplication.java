06fe1a6638d20dd11478c9fe6c3a82c37b024dfe
Murad <81688923+murad34@users.noreply.github.com>
2023-04-20 17:23:57 +0400

    Add files via upload

---
+++ UploadFileJSON/src/main/java/com/example/uploadfilejson/UploadFileJsonApplication.java
@@ -0,0 +1,13 @@
+package com.example.uploadfilejson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadFileJsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadFileJsonApplication.class, args);
+    }
+
+}
---
+++ UploadFileJSON/src/test/java/com/example/uploadfilejson/UploadFileJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uploadfilejson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadFileJsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
